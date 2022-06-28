ba593e14435d623fcab932f1c93981029c578bfa
ZJ <321465580@qq.com>
2022-06-28 10:37:13 +0800

    项目创建

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <p>{{$store.state.moduleA.username}}</p>
+    <p>{{$store.getters.newName}}</p>
+  <!-- namespaced  -->
+    <p>{{$store.state.moduleB.username}}</p>
+    <p>{{$store.getters['moduleB/newName']}}</p>
+    <button @click="fn1">mutations</button>
+    <button @click="fn2">action</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    const fn1 = () => {
+      store.commit('moduleB/updateName')
     }
+    const fn2 = () => {
+      store.dispatch('moduleB/updateName')
+    }
+    return { fn1, fn2 }
   }
 }
+</script>
+
+<style>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,77 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+// 模块A
+const moduleA = {
   state: {
+    username: 'ma'
   },
   getters: {
+    newName (state) {
+      return state.username + '123'
+    }
+
+  }
+}
+// 模块B
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'mb'
+  },
+  getters: {
+    newName (state) {
+      return state.username + '456'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.username = 'asdausiogdhuiga'
+    }
   },
   actions: {
-  },
+    updateName (ctx) {
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 1000)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
+
+// vue2.0 创建仓库 new VueX.store({})
+// vue3.0 创建仓库 createStore({})
+// export default createStore({
+//   // 状态
+//   state: {
+//     user: 'lauid'
+//   },
+//   // 写计算属性
+//   getters: {
+//     newName (state) {
+//       return state.user + '111'
+//     }
+//   },
+//   // 修改方法
+//   mutations: {
+//     updateName (state) {
+//       state.user = 'qweqwe'
+//     }
+//   },
+//   // 拿数据
+//   actions: {
+//     updateName (ctx) {
+//       setTimeout(() => {
+//         ctx.commit('updateName')
+//       }, 1000)
+//     }
+//   },
+//   // 分模块
+//   modules: {
+//   }
+// })
