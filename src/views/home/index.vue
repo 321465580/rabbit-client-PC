43ea5620ba9ecd5cd61e418b8697493b0a08fcc4
ZJ <321465580@qq.com>
2022-07-07 23:52:32 +0800

    实现轮播图, 四个需求

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="container">
    <!-- 首页入口(分类+广告) -->
+  <div class="container">
    <div class="home-entry">
     <!-- 左侧分类 -->
     <HomeCategory />
     <!-- 轮播图 -->
+    <HomeBanner />
    </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'HomePage',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
