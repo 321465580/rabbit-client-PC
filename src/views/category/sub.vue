4b0fdcc62c2936b752e584889820eb006aed74b4
ZJ <321465580@qq.com>
2022-07-23 18:03:50 +0800

    复选框组件封装-监听器实现双向绑定

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,18 +5,27 @@
       <SubBread />
       <!-- 筛选区域 -->
       <SubFilter />
+      <!-- 测试 -->
+      <XtxCheckbox v-model="isAllChecked">全选</XtxCheckbox>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import XtxCheckbox from '@/components/library/xtx-checkbox.vue'
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
 export default {
   name: 'subCategory',
   components: {
     SubBread,
-    SubFilter
+    SubFilter,
+    XtxCheckbox
+  },
+  setup () {
+    const isAllChecked = ref(true)
+    return { isAllChecked }
   }
 }
 </script>
